d3f5dbdc2d2a5b86cf74cd7810d3e2a21955df3b
Emmanuel Joseph (Jet) <emmanueljet774@gmail.com>
2020-03-02 01:05:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,20 +24,18 @@ var rsvpListener = null;
 var guestbookListener = null;
 
 // Add Firebase project configuration object here
-// var firebaseConfig = {};
 const firebaseConfig = {
-    apiKey: "AIzaSyBHye2GyaYLikXnRwaHe6SusmP0UEy_dQg",
-      authDomain: "emmanueljet-fgtk.firebaseapp.com",
-        databaseURL: "https://emmanueljet-fgtk.firebaseio.com",
-          projectId: "emmanueljet-fgtk",
-            storageBucket: "emmanueljet-fgtk.appspot.com",
-              messagingSenderId: "152233201920",
-                appId: "1:152233201920:web:59699b963cfd4e92bce3e5",
-                  measurementId: "G-97NKW0VDBN"
-                  };
-}
+  apiKey: "AIzaSyBHye2GyaYLikXnRwaHe6SusmP0UEy_dQg",
+  authDomain: "emmanueljet-fgtk.firebaseapp.com",
+  databaseURL: "https://emmanueljet-fgtk.firebaseio.com",
+  projectId: "emmanueljet-fgtk",
+  storageBucket: "emmanueljet-fgtk.appspot.com",
+  messagingSenderId: "152233201920",
+  appId: "1:152233201920:web:59699b963cfd4e92bce3e5",
+  measurementId: "G-97NKW0VDBN"
+};
 
-// firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 // FirebaseUI config
 const uiConfig = {
@@ -55,4 +53,31 @@ const uiConfig = {
   }
 };
 
-// const ui = new firebaseui.auth.AuthUI(firebase.auth());
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+// Listen to RSVP button clicks
+startRsvpButton.addEventListener("click",
+ () => {
+      ui.start("#firebaseui-auth-container", uiConfig);
+});
+
+// Listen to the current Auth state
+firebase.auth().onAuthStateChanged((user)=> {
+  if (user) {
+    startRsvpButton.textContent = "LOGOUT"
+  }
+  else {
+    startRsvpButton.textContent = "RSVP"
+  }
+});
+
+startRsvpButton.addEventListener("click",
+ () => {
+    if (firebase.auth().currentUser) {
+      // User is signed in; allows user to sign out
+      firebase.auth().signOut();
+    } else {
+      // No user is signed in; allows user to sign in
+      ui.start("#firebaseui-auth-container", uiConfig);
+    }
+});
